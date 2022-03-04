908f1fc8ae6bb7caa61f16bf69491fb1d0fda18a
Morten W. Jørgensen <Morten@comepile.it>
2022-03-04 13:53:52 +0100

    Added example using a macro

---
+++ as-a-macro/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <speakers_generated_during_build/Hercule.h>
+#include <speakers_generated_during_build/LongFeng.h>
+
+int main(int, char**)
+{
+    Hercule hercule;
+    hercule.speak();
+
+    LongFeng longfeng;
+    longfeng.speak();
+}
